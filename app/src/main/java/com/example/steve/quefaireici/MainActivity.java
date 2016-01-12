97175e0634b2a6d12b05ff21a45b31da65295d40
svandycke <svandycke@gmail.com>
2016-01-12 19:40:42 +0100

    modifications Steve

--- app/src/main/java/com/example/steve/quefaireici/MainActivity.java
+++ app/src/main/java/com/example/steve/quefaireici/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.steve.quefaireici;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import org.json.JSONObject;
 
@@ -10,11 +16,19 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements ListeActivityFragment.OnItemSelectedListener {
 
+    String[] arr = { "Paris, France", "Rambouillet, France","Versailles, France", "Lyon, France", "Chevreuse, France"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AutoCompleteTextView autocomplete = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_item, arr);
+
+        autocomplete.setThreshold(2);
+        autocomplete.setAdapter(adapter);
     }
 
     @Override
