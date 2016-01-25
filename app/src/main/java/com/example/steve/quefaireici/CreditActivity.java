d929284d8b2659ebff53475729b6fa4bf13b278b
Alexandre <alexandre.chevallier4@gmail.com>
2016-01-25 19:51:30 +0100

    credits

---
+++ app/src/main/java/com/example/steve/quefaireici/CreditActivity.java
@@ -0,0 +1,15 @@
+package com.example.steve.quefaireici;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alexandre on 19/01/16.
+ */
+public class CreditActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credit);
+    }
+}
